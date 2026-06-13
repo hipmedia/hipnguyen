8c87d0162c205c457f796280c05232dc486a09e1
hipmedia <hipnguyen545@gmail.com>
2026-06-13 20:29:52 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    // Thay thế URL Web App sau khi deploy Google Apps Script
-    API_URL: "https://script.google.com/macros/s/AKfycbxOZ3_h1Swed1vs0NOytjQgWnVK2HyawYdTepvy0hgzichmJgEb4_eYBTPGijjcgtq-/exec"
-};
